fb25fc41db3dca6c61885c7e9d6fd4b373cccc56
Joaco51976 <joaquincordini62@gmail.com>
2025-08-12 17:11:14 -0300

    Add files via upload

---
+++ Ortoedro.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer1___Act1
+{
+    public class Ortoedro
+    {
+        public decimal Largo { get; set; }
+        public decimal Ancho { get; set; }
+        public decimal Alto { get; set; }
+
+        public Ortoedro(decimal largo, decimal ancho, decimal alto)
+        {
+            Largo = largo;
+            Ancho = ancho;
+            Alto = alto;
+        }
+
+        public List<Rectangulo> Tapas
+        {
+            get
+            {
+                return new List<Rectangulo>
+            {
+                new Rectangulo(Largo, Ancho),
+                new Rectangulo(Largo, Ancho)
+            };
+            }
+        }
+
+        public List<Rectangulo> Laterales
+        {
+            get
+            {
+                return new List<Rectangulo>
+            {
+                new Rectangulo(Alto, Ancho),
+                new Rectangulo(Alto, Ancho),
+                new Rectangulo(Alto, Largo),
+                new Rectangulo(Alto, Largo)
+            };
+            }
+        }
+
+        public decimal Area => 2 * (Largo * Ancho + Largo * Alto + Ancho * Alto);
+        public decimal Volumen => Largo * Ancho * Alto;
+
+        public string Describir()
+        {
+            string tapasJson = string.Join(",\n", Tapas.Select(t => t.Describir()));
+            string lateralesJson = string.Join(",\n", Laterales.Select(l => l.Describir()));
+
+            return $@"{{
+    ""Tipo"": ""Ortoedro"",
+    ""Tapas"": [
+        {tapasJson}
+    ],
+    ""Laterales"": [
+        {lateralesJson}
+    ],
+    ""Area"": {Area:F2},
+    ""Volumen"": {Volumen:F2}
+}}";
+        }
+    }
+}
---
+++ Rectangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer1___Act1
+{
+    public class Rectangulo
+    {
+    public decimal Largo { get; set; }
+    public decimal Ancho { get; set; }
+
+    public Rectangulo(decimal largo, decimal ancho)
+    {
+    Largo = largo;
+    Ancho = ancho;
+    }
+
+    public decimal Area => Largo * Ancho;
+
+        public string Describir()
+        {
+        return $@"{{
+        ""Tipo"": ""Rectangulo"",
+        ""Largo"": {Largo:F2},
+        ""Ancho"": {Ancho:F2},
+        ""Area"": {Area:F2}
+        }}";
+        }
+    }
+}
